d0501ed23444b15f41ea91e887fe5d174f494650
sharmasujata <sharma.sujata64@gmail.com>
2019-08-02 17:28:12 +0530

    New Cahnges

--- Table.java
+++ Table.java
@@ -9,6 +9,8 @@ public class Table {
 		System.out.println("Enter the number");
 		int A=scan.nextInt();
 		
+		System.out.println("Below is the table for " + A);
+		
 		for (int a=1 ; a<=10 ; a++)
 		{
 			System.out.println(a + " * " + A + " = " + (a*A));
